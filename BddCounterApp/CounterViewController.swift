52b44ee724030869dd43784e07e514a856d15cea
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-21 11:09:14 +0900

    SceneDelegateで初期化処理を作成

--- BddCounterApp/CounterViewController.swift
+++ BddCounterApp/CounterViewController.swift
@@ -8,21 +8,27 @@
 import UIKit
 
 final class CounterViewController: UIViewController {
-
+    
     @IBOutlet private weak var countLabel: UILabel!
-    @IBOutlet private weak var plusButton: UIButton!
-    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var decrementButton: UIButton!
+    
+    static func make() -> CounterViewController {
+        let stotyboard = UIStoryboard(name: "Counter",
+                                      bundle: nil)
+        return stotyboard.instantiateInitialViewController() as! CounterViewController
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    @IBAction private func didTapPlusButton(_ sender: Any) {
         
     }
     
-    @IBAction private func didTapMinusButton(_ sender: Any) {
+    @IBAction private func didTapIncrementButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func didTapDecrementButton(_ sender: Any) {
         
     }
     
