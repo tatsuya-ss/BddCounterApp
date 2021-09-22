4f836ddcd49872f8c7ef6989fc87ae732881bcc4
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-23 06:09:36 +0900

    永続化のモックを作成

--- BddCounterApp/CounterViewController.swift
+++ BddCounterApp/CounterViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
+protocol CounterStorage {
+    func save(_ count: Int)
+}
+
+final class CounterStorageMock: CounterStorage {
+    var latestSaveCount: Int?
+    
+    func save(_ count: Int) {
+        latestSaveCount = count
+    }
+}
+
 final class Counter {
     private(set) var count: Int
+    private let counterStorage: CounterStorage!
     
-    init(count: Int = 0) {
+    init(count: Int = 0, counterStorage: CounterStorage? = nil) {
         self.count = count
+        self.counterStorage = counterStorage
     }
     
     var isLowerLimit: Bool { return count == 0 }
@@ -19,10 +33,12 @@ final class Counter {
     
     func increment() {
         count += 1
+        counterStorage?.save(count)
     }
     
     func decrement() {
         count -= 1
+        counterStorage?.save(count)
     }
 }
 
